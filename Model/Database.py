0b60ef5b10dff765f1712118737813e9a0e20f9f
Amine <amine.el-gaouzi@mines-ales.org>
2023-08-24 19:25:04 +0200

    database adjustement

--- Model/Database.py
+++ Model/Database.py
@@ -1,12 +1,14 @@
 import mysql.connector
 
 class MySQLDatabase:
+
     def __init__(self, host, user, password, database):
         self.host = host
         self.user = user
         self.password = password
         self.database = database
         self.connection = None
+        self.connect()
 
     def connect(self):
         try:
