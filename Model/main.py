25aaf2a884227dd0b9495155525d7adbb00567b2
Amine <amine.el-gaouzi@mines-ales.org>
2023-09-04 00:50:09 +0200

    Generate AES key

--- Model/main.py
+++ Model/main.py
@@ -1,10 +1,4 @@
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
 import os
 
-key_length = 16
-key = get_random_bytes(key_length)
-key_hex = key.hex()
-os.environ["MY_ENCRYPTION_KEY"] = key_hex
 encryption_key = bytes.fromhex(os.environ["MY_ENCRYPTION_KEY"])
-print(f'encrypted key :{key} \n environement {encryption_key}')
+print(encryption_key)
