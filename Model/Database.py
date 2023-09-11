5961a75e0bdc043095b7dfe0d91f45a2e14879f4
Amine <amine.el-gaouzi@mines-ales.org>
2023-09-11 18:14:39 +0200

    AppGui

--- Model/Database.py
+++ Model/Database.py
@@ -1,13 +1,17 @@
+import random
+import string
+
 import mysql.connector
+
 
 class MySQLDatabase:
 
-    def __init__(self, host, user, password):
+    def __init__(self, host, user, password, database):
         self.host = host
         self.user = user
         self.password = password
-
         self.connection = None
+        self.database = database
         self.connect()
 
     def connect(self):
@@ -16,6 +20,7 @@ class MySQLDatabase:
                 host=self.host,
                 user=self.user,
                 password=self.password,
+                database=self.database
 
             )
             print("Successfull connection")
@@ -25,14 +30,39 @@ class MySQLDatabase:
     def disconnect(self):
         if self.connection:
             self.connection.close()
-            print("Disconect")
+
 
     def execute_query(self, query):
         try:
             cursor = self.connection.cursor()
             cursor.execute(query)
-            self.connection.commit()
-            print("Query successful excuted")
+
+            if cursor.description is not None:
+
+                result = cursor.fetchall()
+                return result
+            else:
+
+                self.connection.commit()
+                return None
+
         except mysql.connector.Error as err:
-            print("Error when excuting query", err)
-        
+            print("Error when executing query", err)
+            return None
+
+    def insert_data(self, passwords):
+            try:
+                cursor = self.connection.cursor()
+                for website, credentials in passwords.items():
+                    username = credentials['username']
+                    password = credentials['password']
+
+                    query = f"INSERT INTO passwords (website, username, password) VALUES ('{website}', '{username}', '{password}')"
+                    cursor.execute(query)
+                    self.connection.commit()
+
+
+
+                print("inserted successfully.")
+            except mysql.connector.Error as err:
+                print("Error when inserting data", err)
